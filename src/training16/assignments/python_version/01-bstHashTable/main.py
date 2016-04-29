363ca9c8d67212d41798c3cd12c2d02c0ff0a0c9
dannylessio <danny.lessio@gmail.com>
2016-04-29 17:47:00 +0200

    solved assignement n.2

--- src/training16/assignments/python_version/01-bstHashTable/main.py
+++ src/training16/assignments/python_version/01-bstHashTable/main.py
@@ -14,14 +14,42 @@ except ImportError:
 TOTAL_OPERATIONS_NUMBER = list( range( 1000, 101000, 1000 ))
 
 
-### BST IMPLEMENTATION BENCHMARK ---------------------------------------
-print( "STARTING HT_BST BENCHMARK" )
-ht_bst = HashTable( n_buckets=997, implementation="BST" )
+
+### DOUBLE LINKED LIST IMPLEMENTATION BENCHMARK -------------------------
+print( "STARTING HT_LIST BENCHMARK" )
+ht_list = HashTable( n_buckets=251, implementation="LIST" )
 
 # list that holds the time of insert for each n_operations number
-list_time_insert  = []
-list_time_search  = []
-list_time_delete  = []
+ht_list_time_list  = []
+list_alpha_charge  = []
+
+for number_of_operations in TOTAL_OPERATIONS_NUMBER:
+
+	# create a fake user with the right set of operations
+	fake_user_ht_list = HT_Fake_User( hash_table=ht_list, 
+		number_operations=number_of_operations, insert_pergentage=75, 
+		search_pergentage=12.5, delete_pergentage=12.5 )
+
+	fake_user_ht_list.start()
+
+
+	insert_time = fake_user_ht_list.get_total_insert_time() 
+	search_time = fake_user_ht_list.get_total_search_time() 
+	delete_time = fake_user_ht_list.get_total_delete_time()
+	ht_list_time_list.append( insert_time + search_time + delete_time )
+
+	list_alpha_charge.append( ht_list.get_alpha_charge() )
+	print( ht_list.get_alpha_charge() )
+
+
+
+
+### BST IMPLEMENTATION BENCHMARK ---------------------------------------
+print( "STARTING HT_BST BENCHMARK" )
+ht_bst = HashTable( n_buckets=251, implementation="BST" )
+
+# list that holds the time of insert for each n_operations number
+ht_bst_time_list  = []
 list_alpha_charge = []
 
 for number_of_operations in TOTAL_OPERATIONS_NUMBER:
@@ -33,64 +61,26 @@ for number_of_operations in TOTAL_OPERATIONS_NUMBER:
 
 	fake_user_ht_bst.start()
 
-	list_time_insert.append(  fake_user_ht_bst.get_total_insert_time() )
-	list_time_search.append(  fake_user_ht_bst.get_total_search_time() )
-	list_time_delete.append(  fake_user_ht_bst.get_total_delete_time() )
+	insert_time = fake_user_ht_bst.get_total_insert_time() 
+	search_time = fake_user_ht_bst.get_total_search_time() 
+	delete_time = fake_user_ht_bst.get_total_delete_time()
+	ht_bst_time_list.append( insert_time + search_time + delete_time )
+
 	list_alpha_charge.append( ht_bst.get_alpha_charge() )
 	print( ht_bst.get_alpha_charge() )
 
-if matplotlib_available is True:
-	plt.plot( TOTAL_OPERATIONS_NUMBER, list_time_insert, 'y-')
-	plt.plot( TOTAL_OPERATIONS_NUMBER, list_time_search, 'b-')
-	plt.plot( TOTAL_OPERATIONS_NUMBER, list_time_delete, 'g-')
-
-	# patches
-	yellow_patch = mpatches.Patch( color='yellow', label='Insert_time' )
-	blue_patch = mpatches.Patch( color='blue', label='Search_time' )
-	green_patch = mpatches.Patch( color='green', label='Delete_time' )
-	plt.legend( handles=[yellow_patch, blue_patch, green_patch] )
-	plt.xlabel("operation_number")
-	plt.ylabel("time (seconds)")
-	plt.show()
 
 
-
-
-### DOUBLE LINKED LIST IMPLEMENTATION BENCHMARK -------------------------
-print( "STARTING HT_LIST BENCHMARK" )
-ht_list = HashTable( n_buckets=997, implementation="LIST" )
-
-# list that holds the time of insert for each n_operations number
-list_time_insert  = []
-list_time_search  = []
-list_time_delete  = []
-list_alpha_charge = []
-
-for number_of_operations in TOTAL_OPERATIONS_NUMBER:
-
-	# create a fake user with the right set of operations
-	fake_user_ht_list = HT_Fake_User( hash_table=ht_list, 
-		number_operations=number_of_operations, insert_pergentage=75, 
-		search_pergentage=12.5, delete_pergentage=12.5 )
-
-	fake_user_ht_list.start()
-
-	list_time_insert.append(  fake_user_ht_list.get_total_insert_time() )
-	list_time_search.append(  fake_user_ht_list.get_total_search_time() )
-	list_time_delete.append(  fake_user_ht_list.get_total_delete_time() )
-	list_alpha_charge.append( ht_list.get_alpha_charge() )
-	print( ht_list.get_alpha_charge() )
+# PLOT ------------------------------------------------------------------
 
 if matplotlib_available is True:
-	plt.plot( TOTAL_OPERATIONS_NUMBER, list_time_insert, 'y-')
-	plt.plot( TOTAL_OPERATIONS_NUMBER, list_time_search, 'b-')
-	plt.plot( TOTAL_OPERATIONS_NUMBER, list_time_delete, 'g-')
-
+	plt.plot( TOTAL_OPERATIONS_NUMBER, ht_list_time_list, 'b-')
+	plt.plot( TOTAL_OPERATIONS_NUMBER, ht_bst_time_list,  'r-')
+	
 	# patches
-	yellow_patch = mpatches.Patch( color='yellow', label='Insert_time' )
-	blue_patch = mpatches.Patch( color='blue', label='Search_time' )
-	green_patch = mpatches.Patch( color='green', label='Delete_time' )
-	plt.legend( handles=[yellow_patch, blue_patch, green_patch] )
+	blue_patch = mpatches.Patch( color='blue', label='HT_LIST' )
+	red_patch = mpatches.Patch( color='red', label='HT_BST' )
+	plt.legend( handles=[blue_patch, red_patch] )
 	plt.xlabel("operation_number")
 	plt.ylabel("time (seconds)")
 	plt.show()
